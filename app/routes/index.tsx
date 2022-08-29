72a4b5e021ea9d21b70d82c6750e135f9c1c2581
arranfw <arran@freshworks.io>
2022-08-28 20:27:32 -0700

    init remix app, sass, fontawesome

---
+++ app/routes/index.tsx
@@ -0,0 +1,17 @@
+import { WeatherCard } from '~/components/WeatherCard';
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+    </div>
+  );
+}
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: 'https://unpkg.com/modern-css-reset@1.4.0/dist/reset.min.css',
+    },
+  ];
+}
