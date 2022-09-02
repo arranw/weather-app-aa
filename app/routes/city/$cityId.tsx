153bf140b89f21df3bf0b602662032de2dacdb1f
arranw <arranwoodruff@gmail.com>
2022-09-02 11:02:21 -0700

    refactor: remove css reset

--- app/routes/city/$cityId.tsx
+++ app/routes/city/$cityId.tsx
@@ -71,12 +71,3 @@ export default function Index() {
     </div>
   );
 }
-
-export function links() {
-  return [
-    {
-      rel: 'stylesheet',
-      href: 'https://unpkg.com/modern-css-reset@1.4.0/dist/reset.min.css',
-    },
-  ];
-}
